e11da08f6dc1db98317a6578b85a4916fbb40f74
김태형 <th.kim1@greenlabs.co.kr>
2022-06-24 13:12:46 +0900

    1. User Entity Lombok 적용

--- src/main/java/com/greenlabs/day/domain/User.java
+++ src/main/java/com/greenlabs/day/domain/User.java
@@ -1,5 +1,8 @@
 package com.greenlabs.day.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,6 +11,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Getter
+@Setter
 @Entity
 public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,54 +27,6 @@ public class User {
     private int active = 1;
     private String roles = "";
     private String permissions = "";
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getBirth() {
-        return birth;
-    }
-
-    public void setBirth(String birth) {
-        this.birth = birth;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    public void setDisplayName(String display_name) {
-        this.displayName = display_name;
-    }
 
     public List<String> getRoleList(){
         if(this.roles.length()>0){
