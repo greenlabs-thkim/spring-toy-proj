5c9ee1a776f7eeab0f4f1d122ac7e113dfa5c14b
김태형 <th.kim1@greenlabs.co.kr>
2022-06-28 15:39:18 +0900

    user.email 유니크로 변경 불필요한 내용 삭제

--- src/main/java/com/greenlabs/day/domain/User.java
+++ src/main/java/com/greenlabs/day/domain/User.java
@@ -6,29 +6,25 @@ import lombok.ToString;
 
 import javax.persistence.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Getter
 @Setter
 @Entity
 public class User {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String email;
     private String password;
     private String name;
     private String displayName;
     private String birth;
 
-    // 0: deactivate, 1: activate, else: unknown
-    private int active = 1;
-    private String roles = "";
-    private String permissions = "";
-
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @ToString.Exclude
-    private List<Entry> entryList = new ArrayList<Entry>();
+    private List<Entry> entryList = new ArrayList<>();
 
     public void addEntry(Entry entry) {
         entryList.add(entry);
@@ -51,22 +47,6 @@ public class User {
         this.email = email;
         this.password = password;
         this.displayName = displayName;
-    }
-
-    public List<String> getRoleList(){
-        if(this.roles.length()>0){
-            return Arrays.asList(this.roles.split(","));
-        }
-
-        return new ArrayList<>();
-    }
-
-    public List<String> getPermissionList(){
-        if(this.permissions.length()>0){
-            return Arrays.asList(this.permissions.split(","));
-        }
-
-        return new ArrayList<>();
     }
 
     @Override
