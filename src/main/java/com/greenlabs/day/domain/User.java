7a214799770b2381ba9668d8f9a43f7a89713727
김태형 <th.kim1@greenlabs.co.kr>
2022-06-29 09:53:02 +0900

    user.email, password not null

--- src/main/java/com/greenlabs/day/domain/User.java
+++ src/main/java/com/greenlabs/day/domain/User.java
@@ -15,8 +15,9 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
+    @Column(nullable = false)
     private String password;
     private String name;
     private String displayName;
